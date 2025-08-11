1392b57e8e83543c1322c904abb46255e8f0a29d
YongChenSu <yongchensu0@gmail.com>
2025-08-11 14:10:29 +0800

    feat: add new project entry for Creative Coding - Warm Noise with associated image

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -14,12 +14,16 @@ const projectsData: Project[] = [
   },
   {
     title: 'rxjs-playground',
-    description: 'rxjs playground',
-    imgSrc: '/static/images/2025/08/demo.gif',
     description:
       '學習 rxjs 小專案，了解什麼「觀察者 (Observable) 模式」。使用 pokemon 的公開資料，同時用 typescript 實作。',
     imgSrc: '/static/images/2025/08/rxjs-demo.gif',
     href: 'https://github.com/YongChenSu/rxjs-typescript-pokemon?tab=readme-ov-file',
+  },
+  {
+    title: 'Creative Coding - Warm Noise',
+    description: 'Warm Noise, 靈感來自於 Minecraft 中的熔岩，加上 noise 的變化',
+    imgSrc: '/static/images/2025/08/warm-noise.gif',
+    href: 'https://openprocessing.org/sketch/1264279',
   },
 ]
 
