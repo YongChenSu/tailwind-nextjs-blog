5e96f8b45b60a409ff27e35ebf609432ae392964
YongChenSu <yongchensu0@gmail.com>
2025-08-11 14:15:41 +0800

    feat: enhance project description for GoodJob

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,7 +8,7 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'GoodJob 職場透明化運動',
-    description: `超過萬筆薪水、面試資料馬上查！`,
+    description: `你是否曾覺得職場資訊不夠透明？分享你的職場或面試經驗，讓我們一起改變現狀、定義理想的工作！`,
     imgSrc: '/static/images/good-job-share.png',
     href: 'https://github.com/goodjoblife/GoodJobShare',
   },
